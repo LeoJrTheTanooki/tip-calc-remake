3c3874a2bd0c6b6ad3d903c138d220a647193ec7
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-04-03 23:05:14 -0700

    Initialized Repo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,333 @@
+"use client";
+
+import { Button } from "flowbite-react/components/Button";
+import { Card } from "flowbite-react/components/Card";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [billInput, setBillInput] = useState<number>(0);
+  const [peopleInput, setPeopleInput] = useState<number>(0);
+  const [selectedTip, setSelectedTip] = useState<number>(0);
+  const [tipAmount, setTipAmount] = useState<string>("");
+  const [total, setTotal] = useState<string>("");
+
+  const [billInputValue, setBillInputValue] = useState<string>("");
+  const [peopleInputValue, setPeopleInputValue] = useState<string>("");
+  const [customTipInputValue, setCustomTipInputValue] = useState<string>("");
+
+  const [billInputJsx, setBillInputJsx] = useState<HTMLInputElement>();
+  const [peopleInputJsx, setPeopleInputJsx] = useState<HTMLInputElement>();
+  const [customTipInputJsx, setCustomTipInputJsx] =
+    useState<HTMLInputElement>();
+  const [billError, setBillError] = useState<HTMLInputElement>();
+  const [peopleError, setPeopleError] = useState<HTMLInputElement>();
+
+  const [fiveTipBtn, setFiveTipBtn] = useState<HTMLInputElement>();
+  const [tenTipBtn, setTenTipBtn] = useState<HTMLInputElement>();
+  const [fifteenTipBtn, setFifteenTipBtn] = useState<HTMLInputElement>();
+  const [twentyFiveTipBtn, setTwentyFiveTipBtn] = useState<HTMLInputElement>();
+  const [fiftyTipBtn, setfiftyTipBtn] = useState<HTMLInputElement>();
+
+  useEffect(() => {
+    setFiveTipBtn(document.getElementById("fiveTipBtn") as HTMLInputElement);
+    setTenTipBtn(document.getElementById("tenTipBtn") as HTMLInputElement);
+    setFifteenTipBtn(
+      document.getElementById("fifteenTipBtn") as HTMLInputElement
+    );
+    setTwentyFiveTipBtn(
+      document.getElementById("twentyFiveTipBtn") as HTMLInputElement
+    );
+    setfiftyTipBtn(document.getElementById("fiftyTipBtn") as HTMLInputElement);
+
+    setBillInputJsx(
+      document.getElementById("billInputJsx") as HTMLInputElement
+    );
+    setPeopleInputJsx(
+      document.getElementById("peopleInputJsx") as HTMLInputElement
+    );
+    setCustomTipInputJsx(
+      document.getElementById("customTipInputJsx") as HTMLInputElement
+    );
+
+    setBillError(document.getElementById("billError") as HTMLInputElement);
+    setPeopleError(document.getElementById("peopleError") as HTMLInputElement);
+  }, []);
+
+  useEffect(() => {
+    if (selectedTip == 0) {
+      customTipInputJsx?.classList.add(
+        "ring-red-600",
+        "focus:ring-red-600",
+        "ring-2"
+      );
+      customTipInputJsx?.classList.remove("focus:ring-Scyan");
+    } else {
+      customTipInputJsx?.classList.remove(
+        "ring-red-600",
+        "focus:ring-red-600",
+        "ring-2"
+      );
+      customTipInputJsx?.classList.add("focus:ring-Scyan");
+    }
+    handleCalc();
+  }, [billInput, peopleInput, selectedTip]);
+
+  const handleCalc = () => {
+    let handleTip = (billInput * (selectedTip / 100)) / peopleInput;
+    setTipAmount(handleTip.toFixed(2));
+    let handleTotal = (handleTip * peopleInput + billInput) / peopleInput;
+    setTotal(handleTotal.toFixed(2));
+  };
+  const handleTipClick = (tipBtn: any = null) => {
+    let buttons = [
+      fiveTipBtn,
+      tenTipBtn,
+      fifteenTipBtn,
+      twentyFiveTipBtn,
+      fiftyTipBtn,
+    ];
+    for (let i = 0; i < buttons.length; i++) {
+      buttons[i]?.classList.remove("bg-Scyan", "text-VDcyan");
+      buttons[i]?.classList.add("bg-VDcyan", "text-white");
+    }
+
+    tipBtn?.classList.add("bg-Scyan", "text-VDcyan");
+    tipBtn?.classList.remove("bg-VDcyan", "text-white");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="align-middle gap-14 grid w-full justify-center">
+        <div className="grid justify-center pt-10">
+          <img className="" src="/images/logo.svg" alt="" />
         </div>
+        <Card className="text-DGcyan max-w-[1000px] text-sm rounded-2xl">
+          <div className="grid md:grid-cols-2 gap-10">
+            <div className="grid gap-5">
+              <div className="grid grid-cols-2">
+                <div>Bill</div>
+                <div id="billError" className="text-right text-red-600 hidden">
+                  Can't be zero
+                </div>
+                <input
+                  id="billInputJsx"
+                  className="text-2xl col-span-2 text-VDcyan bg-VLGcyan text-right rounded-lg border-0 focus:ring-Scyan focus:ring-2"
+                  type="text"
+                  placeholder="0"
+                  value={billInputValue}
+                  onChange={(e) => {
+                    setBillInput(parseFloat(e.target.value));
+                    setBillInputValue(e.target.value);
+                    if (
+                      billInput < 1 ||
+                      billInputValue == "0" ||
+                      billInputJsx?.value == "0"
+                    ) {
+                      billInputJsx?.classList.add(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      billInputJsx?.classList.remove("focus:ring-Scyan");
+                      billError?.classList.remove("hidden");
+                    } else {
+                      billInputJsx?.classList.remove(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      billInputJsx?.classList.add("focus:ring-Scyan");
+                      billError?.classList.add("hidden");
+                    }
+                  }}
+                  // Code borrowed from Ashur with permission
+                  style={{
+                    backgroundImage: "url(/images/icon-dollar.svg)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "left 10px center",
+                  }}
+                />
+              </div>
+              <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+                <p className="col-span-2 md:col-span-3">Select Tip %</p>
+                <Button
+                  className="bg-VDcyan enabled:hover:bg-[#9ee8dd] enabled:hover:text-VDcyan focus:enabled:bg-Scyan focus:enabled:text-VDcyan focus:ring-0 text-white"
+                  id="fiveTipBtn"
+                  onClick={() => {
+                    setSelectedTip(5);
+                    handleTipClick(fiveTipBtn);
+                  }}
+                >
+                  <span className="text-2xl">5%</span>
+                </Button>
+                <Button
+                  className="bg-VDcyan enabled:hover:bg-[#9ee8dd] enabled:hover:text-VDcyan focus:enabled:bg-Scyan focus:enabled:text-VDcyan focus:ring-0 text-white"
+                  id="tenTipBtn"
+                  onClick={() => {
+                    setSelectedTip(10);
+                    handleTipClick(tenTipBtn);
+                  }}
+                >
+                  <span className="text-2xl">10% </span>
+                </Button>
+                <Button
+                  className="bg-VDcyan enabled:hover:bg-[#9ee8dd] enabled:hover:text-VDcyan focus:enabled:bg-Scyan focus:enabled:text-VDcyan focus:ring-0 text-white"
+                  id="fifteenTipBtn"
+                  onClick={() => {
+                    setSelectedTip(15);
+                    handleTipClick(fifteenTipBtn);
+                  }}
+                >
+                  <span className="text-2xl">15% </span>
+                </Button>
+                <Button
+                  className="bg-VDcyan enabled:hover:bg-[#9ee8dd] enabled:hover:text-VDcyan focus:enabled:bg-Scyan focus:enabled:text-VDcyan focus:ring-0 text-white"
+                  id="twentyFiveTipBtn"
+                  onClick={() => {
+                    setSelectedTip(25);
+                    handleTipClick(twentyFiveTipBtn);
+                  }}
+                >
+                  <span className="text-2xl">25% </span>
+                </Button>
+                <Button
+                  className="bg-VDcyan enabled:hover:bg-[#9ee8dd] enabled:hover:text-VDcyan focus:enabled:bg-Scyan focus:enabled:text-VDcyan focus:ring-0 text-white"
+                  id="fiftyTipBtn"
+                  onClick={() => {
+                    setSelectedTip(50);
+                    handleTipClick(fiftyTipBtn);
+                  }}
+                >
+                  <span className="text-2xl">50% </span>
+                </Button>
+                <input
+                  id="customTipInputJsx"
+                  type="text"
+                  placeholder="Custom"
+                  value={customTipInputValue}
+                  className="text-2xl group relative flex items-stretch justify-center text-right font-medium transition-[color,background-color,border-color,text-decoration-color,fill,stroke,box-shadow] focus:z-10 focus:outline-none dark:focus:ring-cyan-800 rounded-lg bg-VLGcyan enabled:hover:text-VDcyan focus:enabled:text-VDcyan focus:ring-Scyan text-VDcyan focus:ring-2 border-0"
+                  onChange={(e) => {
+                    setSelectedTip(parseFloat(e.target.value));
+                    setCustomTipInputValue(e.target.value);
+                    handleTipClick();
+                    if (
+                      selectedTip < 1 ||
+                      customTipInputValue == "0" ||
+                      customTipInputJsx?.value == "0"
+                    ) {
+                      customTipInputJsx?.classList.add(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      customTipInputJsx?.classList.remove("focus:ring-Scyan");
+                    } else {
+                      customTipInputJsx?.classList.remove(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      customTipInputJsx?.classList.add("focus:ring-Scyan");
+                    }
+                  }}
+                />
+              </div>
+              <div className="grid grid-cols-2">
+                <div>Number of People</div>
+                <div
+                  id="peopleError"
+                  className="text-right text-red-600 hidden"
+                >
+                  Can't be zero
+                </div>
+
+                <input
+                  id="peopleInputJsx"
+                  className="text-2xl col-span-2 text-VDcyan bg-VLGcyan text-right rounded-lg border-0 focus:ring-Scyan focus:ring-2"
+                  type="text"
+                  placeholder="0"
+                  value={peopleInputValue}
+                  onChange={(e) => {
+                    setPeopleInput(parseFloat(e.target.value));
+                    setPeopleInputValue(e.target.value);
+                    if (
+                      peopleInput < 1 ||
+                      peopleInputValue == "0" ||
+                      peopleInputJsx?.value == "0"
+                    ) {
+                      peopleInputJsx?.classList.add(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      peopleInputJsx?.classList.remove("focus:ring-Scyan");
+                      peopleError?.classList.remove("hidden");
+                    } else {
+                      peopleInputJsx?.classList.remove(
+                        "ring-red-600",
+                        "focus:ring-red-600",
+                        "ring-2"
+                      );
+                      peopleInputJsx?.classList.add("focus:ring-Scyan");
+                      peopleError?.classList.add("hidden");
+                    }
+                  }}
+                  // See bill input style
+                  style={{
+                    backgroundImage: "url(/images/icon-person.svg)",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "left 10px center",
+                  }}
+                />
+              </div>
+            </div>
+
+            <div className="bg-VDcyan text-white grid grid-cols-2 p-8 pt-10 gap-y-5 rounded-2xl">
+              <div>
+                <p>Tip Amount</p>
+                <p className="text-xs text-DGcyan">/ person</p>
+              </div>
+              <div className="text-2xl text-Scyan text-right">
+                $
+                {tipAmount && tipAmount !== "NaN" && tipAmount !== "Infinity"
+                  ? tipAmount
+                  : "0.00"}
+              </div>
+              <div>
+                <p>Total</p>
+                <p className="text-xs text-DGcyan">/ person</p>
+              </div>
+              <div className="text-2xl text-Scyan text-right">
+                $
+                {total && total !== "NaN" && total !== "Infinity"
+                  ? total
+                  : "0.00"}
+              </div>
+              <div className="col-span-2 flex items-end">
+                <Button
+                  disabled={
+                    tipAmount && tipAmount !== "NaN" && tipAmount !== "Infinity"
+                      ? false
+                      : true
+                  }
+                  className="w-full bg-Scyan text-VDcyan disabled:bg-[#0d686d] enabled:hover:bg-[#9ee8dd] focus:ring-0"
+                  onClick={() => {
+                    setBillInput(0);
+                    setPeopleInput(0);
+                    setSelectedTip(0);
+                    setBillInputValue("");
+                    setPeopleInputValue("");
+                    setCustomTipInputValue("");
+                  }}
+                >
+                  RESET
+                </Button>
+              </div>
+            </div>
+          </div>
+        </Card>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,24 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {
+      colors: {
+        //Strong Cyan
+        Scyan: "hsl(172, 67%, 45%)",
+        //Very Dark Cyan
+        VDcyan: "hsl(183, 100%, 15%)",
+        //Dark Gray Cyan
+        DGcyan: "hsl(186, 14%, 43%)",
+        //Grayish Cyan
+        Gcyan: "hsl(184, 14%, 56%)",
+        //Light Grayish Cyan
+        LGcyan: "hsl(185, 41%, 84%)",
+        //Very Light Grayish Cyan
+        VLGcyan: "hsl(189, 41%, 97%)",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -15,6 +30,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("flowbite/plugin"),
+  ],
 };
 export default config;
